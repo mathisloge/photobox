263842a44465b2c529f72e0b52e3c382090f154c
Mathis Logemann <mathisloge@gmail.com>
2024-09-16 20:03:41 +0200

    add collage editor

--- Photobox/CollageEditorApp/main.cpp
+++ Photobox/CollageEditorApp/main.cpp
@@ -14,16 +14,11 @@ Q_IMPORT_QML_PLUGIN(Photobox_UiPlugin)
 
 using namespace Pbox;
 
-namespace
-{
-void testPrint();
-}
-
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
     QCoreApplication::setApplicationName(QStringLiteral("PhotoBox"));
-    QCoreApplication::setOrganizationName(QStringLiteral("com.mathisloge.photobox"));
+    QCoreApplication::setOrganizationName(QStringLiteral("com.mathisloge-photobox.collage"));
     QCoreApplication::setApplicationVersion(QStringLiteral(QT_VERSION_STR));
 
     std::shared_ptr<PhotoTriggerClient> photo_trigger_client = std::make_shared<PhotoTriggerClient>();
@@ -41,47 +36,7 @@ int main(int argc, char *argv[])
     app_state->camera = camera;
     app_state->trigger_client = photo_trigger_client;
 
-    engine.loadFromModule("Photobox.App", "Main");
-    engine.addImageProvider(QLatin1String("camera"), new CameraImageProvider(camera)); // qml engine takes ownership
-
-    testPrint();
+    engine.loadFromModule("Photobox.CollageEditorApp", "Main");
 
     return app.exec();
 }
-
-#include <QPainter>
-#include <QPrintDialog>
-#include <QPrinter>
-namespace
-{
-void testPrint()
-{
-    QPrinter printer(QPrinter::HighResolution);
-    printer.setOutputFileName("print.pdf");
-
-    QPainter painter;
-    painter.begin(&printer);
-
-    int numberOfPages = 2;
-    int lastPage = numberOfPages - 1;
-
-    for (int page = 0; page < numberOfPages; ++page)
-    {
-
-        painter.save();
-        painter.drawRect(10, 10, 100, 100);
-        painter.setPen(Qt::blue);
-        painter.setFont(QFont("Arial", 30));
-        painter.drawText(100, 100, "Qt");
-
-        painter.restore();
-
-        if (page != lastPage)
-            printer.newPage();
-    }
-
-    painter.end();
-
-    printer.setOutputFormat(QPrinter::OutputFormat::PdfFormat);
-}
-} // namespace
