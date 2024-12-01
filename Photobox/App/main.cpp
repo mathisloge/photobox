452ea4f6ff184ece46723cf5adc72389703c9c42
Mathis Logemann <mathisloge.opensource@pm.me>
2024-12-01 12:54:53 +0100

    use default collage session

--- Photobox/App/main.cpp
+++ Photobox/App/main.cpp
@@ -101,9 +101,9 @@ int main(int argc, char *argv[])
         SystemStatusManager system_status_manager;
 
         ImageStorage image_storage{capture_directory.toStdString()};
-        // CollageContext collage_context{
-        //     scheduler, image_storage, collage_directory.toStdString(), printer_settings.toStdString()};
-        // system_status_manager.registerClient(std::addressof(collage_context.systemStatusClient()));
+        CollageContext collage_context{
+            scheduler, image_storage, collage_directory.toStdString(), printer_settings.toStdString()};
+        system_status_manager.registerClient(std::addressof(collage_context.systemStatusClient()));
 
         std::unique_ptr<RemoteTrigger> remote_trigger =
             std::make_unique<EspHomeRemoteTrigger>(std::make_unique<EspHomeClient>(trigger_button_host));
@@ -120,9 +120,10 @@ int main(int argc, char *argv[])
             camera = std::make_shared<MockCamera>();
         }
 
-        CaptureManager capture_manager{scheduler, image_storage, *camera, *remote_trigger, *camera_led, [] {
-                                           return std::make_unique<SingleCaptureSession>();
-                                       }};
+        CaptureManager capture_manager{
+            scheduler, image_storage, *camera, *remote_trigger, *camera_led, [&collage_context] {
+                return std::make_unique<CollageCaptureSession>(collage_context);
+            }};
         system_status_manager.registerClient(std::addressof(camera->systemStatusClient()));
 
         QQmlApplicationEngine engine;
