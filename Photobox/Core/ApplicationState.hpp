c76e44fe6d91da34328fbcc09ed98bf4d89fdeb8
Mathis Logemann <13556116+mathisloge@users.noreply.github.com>
2025-07-17 22:04:34 +0200

    fix all license headers

--- Photobox/Core/ApplicationState.hpp
+++ Photobox/Core/ApplicationState.hpp
@@ -1,5 +1,5 @@
-// SPDX-FileCopyrightText: 2024 Mathis Logemann <mathisloge.opensource@pm.me>
-// SPDX-FileCopyrightText: 2025 Mathis Logemann <mathisloge.opensource@pm.me>
+// SPDX-FileCopyrightText: 2024 Mathis Logemann <mathis.opensource@tuta.io>
+// SPDX-FileCopyrightText: 2025 Mathis Logemann <mathis.opensource@tuta.io>
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
