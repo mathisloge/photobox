c85106e51def9e03b613aaed8f6ae94f72b5c337
Mathis Logemann <13556116+mathisloge@users.noreply.github.com>
2025-07-17 22:04:34 +0200

    fix all license headers

--- Tests/CaptureSessions/SingleCaptureSessionTest.cpp
+++ Tests/CaptureSessions/SingleCaptureSessionTest.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2024 Mathis Logemann <mathisloge.opensource@pm.me>
-// SPDX-FileCopyrightText: 2025 Mathis Logemann <mathisloge.opensource@pm.me>
+// SPDX-FileCopyrightText: 2024 Mathis Logemann <mathis.opensource@tuta.io>
+// SPDX-FileCopyrightText: 2025 Mathis Logemann <mathis.opensource@tuta.io>
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
