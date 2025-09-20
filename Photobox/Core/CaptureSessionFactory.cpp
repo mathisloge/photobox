d8f9157598eccab5b86e965b9b20f74330601b06
Mathis Logemann <13556116+mathisloge@users.noreply.github.com>
2025-09-20 17:47:10 +0200

    add license headers

--- Photobox/Core/CaptureSessionFactory.cpp
+++ Photobox/Core/CaptureSessionFactory.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Mathis Logemann <mathis.opensource@tuta.io>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include "CaptureSessionFactory.hpp"
 #include "ICaptureSession.hpp"
 #include "IdleCaptureSession.hpp"
--- Photobox/Core/CollageCaptureSessionFactory.cpp
+++ Photobox/Core/CollageCaptureSessionFactory.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Mathis Logemann <mathis.opensource@tuta.io>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include "CollageCaptureSessionFactory.hpp"
 #include "CollageCaptureSession.hpp"
 #include "Pbox/CleanupAsyncScope.hpp"
