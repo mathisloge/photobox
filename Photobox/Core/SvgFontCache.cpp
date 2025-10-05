09daafd0c6f48e318b3b5a8f921f921a84dd4c05
Mathis Logemann <13556116+mathisloge@users.noreply.github.com>
2025-10-05 17:13:01 +0200

    feature: relative paths are resolved from the settings root

--- Photobox/Core/SvgFontCache.cpp
+++ Photobox/Core/SvgFontCache.cpp
@@ -12,19 +12,24 @@ namespace Pbox
 
 void SvgFontCache::registerFont(std::string family, bool bold, bool italic, std::filesystem::path path)
 {
-    auto &&[font, emplaced] =
-        registered_fonts_.emplace(Detail::RegisteredFont{.family = std::move(family), .bold = bold, .italic = italic});
+    auto &&[font, emplaced] = registered_fonts_.emplace(
+        Detail::RegisteredFont{.family = std::move(family), .bold = bold, .italic = italic, .path = std::move(path)});
 
-    LOG_DEBUG(
-        logger_svg_font_cache(), "Register font '{}', bold={}, italic={}.", font->family, font->bold, font->italic);
+    LOG_DEBUG(logger_svg_font_cache(),
+              "Register font '{}', bold={}, italic={} from '{}'",
+              font->family,
+              font->bold,
+              font->italic,
+              font->path.string());
     if (emplaced)
     {
-        lunasvg_add_font_face_from_file(font->family.c_str(), font->bold, font->italic, path.c_str());
+        lunasvg_add_font_face_from_file(font->family.c_str(), font->bold, font->italic, font->path.c_str());
     }
     else
     {
         LOG_WARNING(logger_svg_font_cache(),
-                    "Font '{}', bold={}, italic={} was already registered.",
+                    "Font '{}' from '{}', bold={}, italic={} was already registered.",
+                    font->path.string(),
                     font->family,
                     font->bold,
                     font->italic);
