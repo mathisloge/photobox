f458b33fed6a1814e783537764d85cad6e6d15f9
Mathis Logemann <13556116+mathisloge@users.noreply.github.com>
2025-10-05 14:47:37 +0200

    feature: update & run pre-commit

--- Photobox/Core/SvgFontCache.cpp
+++ Photobox/Core/SvgFontCache.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Mathis Logemann <mathis.opensource@tuta.io>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include "SvgFontCache.hpp"
 #include <Pbox/Logger.hpp>
 #include <lunasvg.h>
--- Photobox/Core/SvgFontCache.hpp
+++ Photobox/Core/SvgFontCache.hpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Mathis Logemann <mathis.opensource@tuta.io>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #pragma once
 #include <filesystem>
 #include <string>
